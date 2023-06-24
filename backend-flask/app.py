3bb71db371594c4417f607bcb5751066d04e8909
Tariq Ahmad <54164634+tariqsheikhsw@users.noreply.github.com>
2023-06-24 06:49:57 +0400

    Update app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,7 +22,17 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+# Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
 app = Flask(__name__)
+# OpenTelemetry / HoneyComb
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
